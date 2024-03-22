5a2b6915e43506b4b3f7cdadf3ca0ffb6efb20ca
Ayush Sharma <arksharma06@outlook.com>
2024-03-22 10:39:22 +0530

    added update api

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ const Product = require('./models/product.model.js');
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://arksharma06:Maamaa123@mongodb.wbdpi3s.mongodb.net/node-api?retryWrites=true&w=majority&appName=MongoDB')
-.then(()=>{
-    console.log("Connected to database");
-    app.listen(3000,()=>{
-        console.log('Server running on 3000');
-    });
-})
-.catch(()=>{
-    console.log("Unsuccessful Connection");
-})
 
 app.get('/api/product/:id',async(req,res)=>{
     try {
@@ -50,5 +40,33 @@ app.post('/api/products',async(req,res)=>{
     } catch (error) {
         res.status(500).json({message:error.message});
     }
+})
+
+//updating product
+app.put('/api/product/:id',async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id,req.body);
+
+        if (!product) {
+            return res.status(404).json({message: 'Product not found'})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+mongoose.connect('mongodb+srv://arksharma06:Maamaa123@mongodb.wbdpi3s.mongodb.net/node-api?retryWrites=true&w=majority&appName=MongoDB')
+.then(()=>{
+    console.log("Connected to database");
+    app.listen(3000,()=>{
+        console.log('Server running on 3000');
+    });
+})
+.catch(()=>{
+    console.log("Unsuccessful Connection");
 })
 
