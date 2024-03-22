04c659282bca56a1b361151a4c3f1a73e527c9af
Ayush Sharma <arksharma06@outlook.com>
2024-03-22 10:10:03 +0530

    created an api to retrieve product by id

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ mongoose.connect('mongodb+srv://arksharma06:Maamaa123@mongodb.wbdpi3s.mongodb.ne
     console.log("Unsuccessful Connection");
 })
 
+app.get('/api/product/:id',async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
 app.get('/api/products',async(req,res)=>{
     try {
         const products = await Product.find({});
